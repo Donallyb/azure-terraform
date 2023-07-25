de1fcc0131cb8770375c77a0d09470342d05926c
Donal Bolger <donalbolger@gmail.com>
2023-07-25 18:05:10 +0100

    Adding Terraform Azure Files

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "host_os" {
+  type = string
+}
+
+variable "azure_location" {
+  type = string
+}
+
+variable "public_ssh_key" {
+  type = string
+}
+
+variable "private_ssh_key" {
+  type = string
+}
+
+variable "source_ip_address" {
+  type = string
+}
+
